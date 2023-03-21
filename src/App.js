16c36c26bd8b146844e81d4d96623adaaaf188c1
LC-GHub <2717891c@student.gla.ac.uk>
2023-03-21 17:02:27 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -2,62 +2,42 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import map from './map.png';
 import './App.css';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
 
 function App() {
   const [coordinates, setCoordinates] = useState({ x: '', y: '' });
   const [selectedItem, setSelectedItem] = useState('');
   const [selectedGridCoordinates, setSelectedGridCoordinates] = useState({ x: null, y: null });
-  const [data, setData] = useState({});
+  const [data, setData] = useState();
   const [intervalId, setIntervalId] = useState(null); // Store intervalId in state
-  let x = 0;
-  let y = 0;
 
-  useEffect(() => {
-    return () => {
-      // Clean up the interval when the component unmounts or the selectedItem changes
-      clearInterval(intervalId);
-    };
-  }, [intervalId, selectedItem]);
+  // const data = {"endoscopeShyam": {"x": 1, "y": 4}, "endoscopeJeff": {"x": 5, "y": 2}}
+  // const endoscopeJeffX = data.endoscopeJeff.x;
+  // console.log(endoscopeJeffX); // prints 5
+  
 
   const handleCoordinateChange = (event, axis) => {
     const newCoordinates = { ...coordinates, [axis]: event.target.value };
     setCoordinates(newCoordinates);
   };
 
-  const handleItemSelection = (event) => {
-    const selectedItem = event.target.value;
-    setSelectedItem(selectedItem);
+  const id = setInterval(() => {
+    axios.get('http://192.168.229.162:8015/data')
+      .then(response => {
+        setData(response.data)
+        setSelectedGridCoordinates({ x: parseInt(response.data.endoscopeShyam.x), y: parseInt(response.data.endoscopeShyam.y) });
+        setCoordinates({ x: response.data.endoscopeShyam.x, y: response.data.endoscopeShyam.y }); // Update X and Y coordinates
 
-    clearInterval(intervalId); // Clear the previous interval
-    // Set selected grid coordinates based on selected item
-    switch (selectedItem) {
-      case 'A':
-        const id = setInterval(() => {
-          // x = x + 1;
-          // y = y + 1;
-          // setSelectedGridCoordinates({ x: x, y: y });
-          // setCoordinates({ x: x, y: y });
-          axios.get('http://192.168.229.162:8015/data')
-            .then(response => {
-                setSelectedGridCoordinates({ x: parseInt(response.data.x), y: parseInt(response.data.y) });
-                setCoordinates({ x: response.data.x, y: response.data.y }); // Update X and Y coordinates
-              })
-            .catch(error => console.log(error));
-        }, 5000);
-        setIntervalId(id); // Store the new intervalId in state
-        break;
-      case 'B':
-        setSelectedGridCoordinates({ x: 8, y: 2 });
-        setCoordinates({ x: '1.3712772', y: '103.8394046' });
-        break;
-      case 'C':
-        setSelectedGridCoordinates({ x: 3, y: 7 });
-        setCoordinates({ x: '1.3591946', y: '103.8270986' });
-        break;
-      default:
-        setSelectedGridCoordinates({ x: null, y: null });
-    }
-  };
+        })
+      .catch(error => console.log(error));
+  }, 5000);
 
 
   return (
@@ -76,33 +56,36 @@ function App() {
             {[...Array(10)].map((_, rowIndex) => (
               <div className="grid-row" key={rowIndex}>
                 {[...Array(10)].map((_, colIndex) => (
-                  <div
-                    className={`grid-cell${
-                      selectedGridCoordinates.x === colIndex && selectedGridCoordinates.y === rowIndex ? ' selected' : ''
-                    }`}
-                    key={`${rowIndex}-${colIndex}`}
-                  />
+                    <div
+                      // className={`grid-cell${
+                      //   data.endoscopeShyam.x === colIndex && data.endoscopeShyam.y === rowIndex ? ' selected' : ''                      
+                      // }`}
+                      className={`grid-cell${
+                        data.endoscopeShyam.x === colIndex && data.endoscopeShyam.y === rowIndex ? ' selected' : ''
+                      }${
+                        data.endoscopeJeff.x === colIndex && data.endoscopeJeff.y === rowIndex ? ' selected1' : ''
+                      }${
+                        data.endoscopeShyam.x === colIndex && data.endoscopeShyam.y === rowIndex && data.endoscopeJeff.x === colIndex && data.endoscopeJeff.y === rowIndex ? ' sameSelected' : ''
+                      }`}
+                      key={`${rowIndex}-${colIndex}`}
+                    />
                 ))}
               </div>
             ))}
           </div>
         </div>
-        <div className="item-list">
-          <label>Choose an Endoscope:</label>
-          <br />
-          <label>
-            <input type="radio" name="item" value="A" checked={selectedItem === 'A'} onChange={handleItemSelection} /> Endoscope A
-          </label>
-          <br />
-          <label>
-            <input type="radio" name="item" value="B" checked={selectedItem === 'B'} onChange={handleItemSelection} /> Endoscope B
-          </label>
-          <br />
-          <label>
-            <input type="radio" name="item" value="C" checked={selectedItem === 'C'} onChange={handleItemSelection} /> Endoscope C
-          </label>
-          <br />
-        </div>
+        <Box>
+        Legend:
+          <List>
+              <ListItem className='text'>
+              endoscopeShyam
+              </ListItem>
+              <ListItem className='text1'>
+              endoscopeJeff
+              </ListItem>
+            </List>
+        </Box>
+
       </div>
     </div>
   </div>
